2b8b35eb6238c9772a9234510d7f49ea1db9d0e3
vlad86122 <145791526+vlad86122@users.noreply.github.com>
2023-10-03 20:42:22 +0300

    add file insdex.js

---
+++ index.js
@@ -0,0 +1,42 @@
+//Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+//Задание 2
+let firstiPhoneYear = 2007;
+alert(firstiPhoneYear);
+//Задание 3
+let javascriptCreatorName = (`Брендан Айк`);
+alert(javascriptCreatorName);
+//Задание 4
+let c = 10;
+let b = 2;
+alert(`Сумма: ${c + b} Разность: ${c - b} Произведение: ${c * b} Частное: ${c / b}`);
+//Задание 5
+let result = Math.pow(2, 5);
+alert(result);
+//Задание 6
+alert(9 % 2);
+//Задание 7
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+//Задание 8
+var age = prompt(`Сколько вам лет?`);
+alert(`Вам ${age} лет.`);
+//Задание 9
+const user = {
+name: 'vlad', 
+age: 21,
+isAdmin: false,
+};
+//Задание 10
+let userName;
+userName = prompt(`Как вас зовут?`);
+alert(`Привет, ${userName}  !`)
