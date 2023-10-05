111d0b9c9ae4feabb505921ece77077cb9e7db73
vlad86122 <145791526+vlad86122@users.noreply.github.com>
2023-10-05 15:30:34 +0300

    homework

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Урок 2.2
 //Задание 1
 let a = 10;
 alert(a);
@@ -74,3 +75,64 @@ if (info == "age") {
 let userName;
 userName = prompt(`Как вас зовут?`);
 alert(`Привет, ${userName}  !`)
+
+//Урок 2.3
+
+//Задание 1
+let password = `пароль`;
+let userInput = prompt(`Введите пароль`);
+if (userInput === password) {
+  console.log("Пароль введен верно");
+} else {
+  console.log("Пароль введен неправильно");
+}
+
+//Задание 2
+let d = 2;
+if (d > 0 && d < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+//Задание 3
+let e = 50;
+let f = 150;
+
+if (e > 100 || f > 100) {
+  console.log('Верно');
+} else {
+  console.log('Неверно');
+}
+
+//Задание 4
+let g = '2';
+let j = '3';
+alert((+g) + (+j));
+
+//Задание 5
+let monthNumber = 4;
+switch (monthNumber) {
+case 1:
+case 2:
+case 12:
+  console.log(`Месяц ${monthNumber} относится к зиме.`);
+  break;
+case 3:
+case 4:
+case 5:
+  console.log(`Месяц ${monthNumber} относится к весне.`);
+  break;
+case 6:
+case 7:
+case 8:
+  console.log(`Месяц ${monthNumber} относится к лету.`);
+  break;
+case 9:
+case 10:
+case 11:
+  console.log(`Месяц ${monthNumber} относится к осени.`);
+  break;
+default:
+  console.log(`Такого месяца не существует.`);
+}
